bb75e9dedb16064afcc0d74972e7eac9c7d7c8b6
Stockgolm <60109023+Stockgolm@users.noreply.github.com>
2024-07-02 23:37:23 +0300

    Update content.js

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 (function() {
-  const url = "https://energy-ua.info/grafik/%D1%81%D0%BC%D1%82+%D0%9D.%D0%A1%D0%B0%D0%BD%D0%B6%D0%B0%D1%80%D0%B8/%D0%93%D0%BE%D1%80%D0%B1%D0%B5%D0%BD%D0%BA%D0%B0";
+  const url = "тут ваша url";
   const updateInterval = 1000000;
   const container = document.createElement('div');
   container.id = 'light-outage-timer';
